d8b04ef9b2b8e6562f37edf660b99b22311cd226
josh mitchell <jshmtchll@gmail.com>
2021-02-14 16:08:42 -0600

    usser confirmations into userSelection array

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-// the arryas of characters
+// strings for user confirmations
 var specialCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
 var lowerCase = "abcdefghijklmnopqrstuvwxyz"
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -8,63 +8,66 @@ var numbers = "123456789"
 
 // Assignment code here
 function generatePassword () {
-  var passwordLength = window.prompt("How long would you like your password to be? Choose between 8 and 128.");
-  console.log(passwordLength);
-//sets passwords length
-  if (passwordLength > 7 && passwordLength < 129) {
-    window.alert("Your password will have " + passwordLength + " characters." );
-    //windows to confirm what characters to use. true or false.
-    var  confirmSpecialCharacters = window.confirm("Would you like to include special characters in the password?");
-    console.log(confirmSpecialCharacters);
+      var passwordLength = parseInt(window.prompt("How long would you like your password to be? Choose between 8 and 128."));
+      console.log(passwordLength);
+       //sets passwords length
+      if (passwordLength > 7 && passwordLength < 129) {
+        window.alert("Your password will have " + passwordLength + " characters." );
+        //windows to confirm what characters to use. true or false.
+        var  confirmSpecialCharacters = window.confirm("Would you like to include special characters in the password?");
+        console.log(confirmSpecialCharacters);
 
-    var  confirmNumbers = window.confirm("Would you like to use numbers in your password?");
-    console.log(confirmNumbers);
+        var  confirmNumbers = window.confirm("Would you like to use numbers in your password?");
+        console.log(confirmNumbers);
 
-    var confirmUppercase = window.confirm("Would you like to use uppercase letters?");
-    console.log(confirmUppercase);
+        var confirmUppercase = window.confirm("Would you like to use uppercase letters?");
+        console.log(confirmUppercase);
 
-    var confirmLowercase = window.confirm("Would you like to use lowercase letters?");
-    console.log(confirmLowercase);
+        var confirmLowercase = window.confirm("Would you like to use lowercase letters?");
+        console.log(confirmLowercase);
 
-    
-    var userPassword = []
-    
-    if (confirmSpecialCharacters) {
-      userPassword = specialCharacters.split('') //if true on confirm window, pulls from corresponding array to go to var userPassword
-      console.log(userPassword);
+        //userSelection stores characters user slected for password from above window.confirms
+        var userSelection = []
+        
+        if (confirmSpecialCharacters) {
+          userSelection += specialCharacters //if true on confirm window, pulls from corresponding string and adds to var user 
+          console.log(userSelection);
+        }
+
+        if (confirmNumbers) {
+          userSelection += numbers.split('') //if true on confirm window, pulls from corresponding string and adds to var user 
+          console.log(userSelection);
+        }
+
+        if (confirmUppercase) {
+          userSelection += upperCase.split('') //if true on confirm window, pulls from corresponding string and adds to var user 
+          console.log(userSelection);
+        }
+        
+        if (confirmLowercase) {
+          userSelection += lowerCase.split('') //if true on confirm window, pulls from corresponding string and adds to var user 
+          console.log(userSelection);
+        }
+
+        console.log(userSelection);
+
+        var userPassword = ""
+
+        for (var i = 0; i < passwordLength; i++) {
+          userPassword = userPassword + userSelection[Math.floor(Math.random() * userSelection.length)];
+        }
+
+        return userPassword;
+
+        
+        
+
+
+
     }
-
-    if (confirmNumbers) {
-      userPassword = numbers.split('')
-      console.log(userPassword);
-    }
-
-    if (confirmUppercase) {
-      userPassword = upperCase.split('')
-      console.log(userPassword);
-    }
-    
-    if (confirmLowercase) {
-      userPassword = lowerCase.split('')
-      console.log(userPassword);
-    }
-
-    console.log(userPassword);
-
-
-
-  } 
 
   else window.alert("Please pick a password length between 8 and 128 characters.");
   generatePassword();
-
-
-
-  
-
-  
-
-
 
 }
 
