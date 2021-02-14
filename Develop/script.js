43f4f8e6136d67b4203b9c713718c655d36e4111
josh mitchell <jshmtchll@gmail.com>
2021-02-13 22:53:16 -0600

    added capture for user input

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,66 @@
+// the arryas of characters
+var specialCharacters = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"]
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+var numbers = ["123456789"]
+
+
+
 // Assignment code here
 function generatePassword () {
   var passwordLength = window.prompt("How long would you like your password to be? Choose between 8 and 128.");
   console.log(passwordLength);
-
+//sets passwords length
   if (passwordLength > 7 && passwordLength < 129) {
     window.alert("Your password will have " + passwordLength + " characters." );
+
+    var  confirmSpecialCharacters = window.confirm("Would you like to include special characters in the password?");
+    console.log(confirmSpecialCharacters);
+
+    var  confirmNumbers = window.confirm("Would you like to use numbers in your password?");
+    console.log(confirmNumbers);
+
+    var confirmUppercase = window.confirm("Would you like to use uppercase letters?");
+    console.log(confirmUppercase);
+
+    var confirmLowercase = window.confirm("Would you like to use lowercase letters?");
+    console.log(confirmLowercase);
+
+    //user responses being fed into this array
+    var userPassword = []
+    
+    if (confirmSpecialCharacters) {
+      userPassword = specialCharacters //will go to userpassword variable
+      console.log(userPassword);
+    }
+
+    if (confirmNumbers) {
+      userPassword = numbers
+      console.log(userPassword);
+    }
+
+    if (confirmUppercase) {
+      userPassword = upperCase
+      console.log(userPassword);
+    }
+    
+    if (confirmLowercase) {
+      userPassword = lowerCase
+      console.log(userPassword);
+    }
+
+
+
   } 
 
-  else generatePassword();
+  else window.alert("Please pick a password length between 8 and 128 characters.");
+  generatePassword();
+
+
+
+  
+
+  
 
 
 
@@ -31,11 +84,6 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// the arryas of characters
 
-var specialCharacters = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
-var lowerCase = ["abcdefghijklmnopqrstuvwxyz"]
-var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
-var numbers = ["123456789"]
 
 
